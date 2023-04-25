4a6eecb0893c2f3a1c86786d9938f767f631c92c
Sanket <kasrsanket@gmail.com>
2023-04-26 03:30:26 +0530

    API integration in UserProfile and Registeration

--- app/src/main/java/com/example/sportsapp/Registration.java
+++ app/src/main/java/com/example/sportsapp/Registration.java
@@ -6,30 +6,39 @@ import java.util.regex.*;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Registration extends AppCompatActivity {
     EditText pass,cpass,name,dob,user;
     FirebaseAuth auth;
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = auth.getCurrentUser();
-        if(currentUser != null){
-            Intent i=new Intent(getApplicationContext(), FirstPage.class);
-            startActivity(i);
-            finish();
-        }
-    }
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        FirebaseUser currentUser = auth.getCurrentUser();
+//        if(currentUser != null){
+//            Intent i=new Intent(getApplicationContext(), FirstPage.class);
+//            startActivity(i);
+//            finish();
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +81,11 @@ public class Registration extends AppCompatActivity {
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
                                         Toast.makeText(getApplicationContext(), "Registration Successful", Toast.LENGTH_SHORT).show();
+                                        try {
+                                            callAPI(userID,nm,date);
+                                        } catch (JSONException e) {
+                                            throw new RuntimeException(e);
+                                        }
                                         Intent i=new Intent(getApplicationContext(),Login.class);
                                         startActivity(i);
                                         finish();
@@ -96,5 +110,26 @@ public class Registration extends AppCompatActivity {
 
         }
 
+    }
+    public void callAPI(final String email, final String name, String dob) throws JSONException {
+        RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
+        String url = "https://cricketappserver.onrender.com/register"; // <----enter your post url here
+        JSONObject object = new JSONObject();
+        object.put("email",email);
+        object.put("name",name);
+        object.put("dob",dob);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, object,
+                (response -> {
+                    try{
+                        Log.d("api",String.valueOf(response));
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                })
+                ,(error -> {
+            Log.e("Error","Error"+error.getMessage());
+        }));
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(jsonObjectRequest);
     }
 }
